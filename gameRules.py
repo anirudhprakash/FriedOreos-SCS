9b3dcf8ffced8fa6ab42ab0b23d8f9e43f297e73
anirudhprakash <anirudh.prakash1@gmail.com>
2021-11-06 18:23:13 -0400

    Update gameRules.py

--- gameRules.py
+++ gameRules.py
@@ -62,6 +62,52 @@ def generateMap(dimensions):
 
 
 
+from collections import deque
+
+
+def checkMap(board):
+    badSquares = ['Ocean', 'Mountain']
+    matrix = []
+    validSquares = []
+    for i in range(len(board)):
+        matrix.append([])
+        for j in range(len(board)):
+            if board[i][j] in badSquares:
+                matrix[i].append(120)
+            else:
+                matrix[i].append(1)
+                validSquares.append((i, j))
+    
+    for i in range(len(validSquares)):
+        for j in range(i, len(validSquares)):
+            start = validSquares[i]
+            end = validSquares[j]
+            if BFS(start, end, matrix) == -1:
+                return False
+    return True
+
+#TODO Modify this BFS to my BFS
+def bfs(grid, start):
+    queue = collections.deque([[start]])
+    seen = set([start])
+    while queue:
+        path = queue.popleft()
+        x, y = path[-1]
+        if grid[y][x] == goal:
+            return path
+        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+            if 0 <= x2 < width and 0 <= y2 < height and grid[y2][x2] != wall and (x2, y2) not in seen:
+                queue.append(path + [(x2, y2)])
+                seen.add((x2, y2))
+
+
+def BFS(start, end, graph):
+    queue = dequeu(start)
+    seen = set([start])
+
+
+
+    
 
 
 
