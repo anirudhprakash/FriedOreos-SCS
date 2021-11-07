8614436dd3e1c6db7107e95c5333f8e15d7b8968
Yumptydumpty <91789460+Yumptydumpty@users.noreply.github.com>
2021-11-06 22:40:04 -0400

    Add files via upload

--- MapQuest 112.py
+++ MapQuest 112.py
@@ -4,16 +4,16 @@ def appStarted(app):
 
     #board
     app.board = [
-                   ['BaseKosbie','Grass','OutpostKosbie','Grass','Grass','Grass','Ocean','Ocean'],
-                   ['Grass','Grass','Grass','Farm','Grass','Farm','Grass','Ocean'],
-                   ['OutpostKosbie','Grass','Grass','Grass','Grass','Grass','Outpost','Ocean'],
-                   ['Grass','Grass','Farm','Grass','Outpost','Grass','Grass','Ocean'],
-                   ['Grass','Grass','Grass','Mountain','Grass','Grass','Farm','Grass'],
-                   ['Grass','Grass','Grass','Mountain','Grass','Grass','Farm','OutpostTaylor'],
-                   ['Mountain','Outpost','Grass','Farm','Grass','Grass','Grass','Grass'],
-                   ['Grass','Mountain','Grass','Grass','Grass','OutpostTaylor','Grass','BaseTaylor']
+                   ['BaseKosbie','Grass','OutpostKosbie','Grass','Grass','Ocean'],
+                   ['Grass','Grass','Grass','Farm','Grass','Ocean'],
+                   ['OutpostKosbie','Grass','Grass','Grass','Grass','Ocean'],
+                   ['Grass','Grass','Farm','Grass','Outpost','Grass'],
+                   ['Grass','Grass','Grass','Mountain','Grass','Grass'],
+                   ['Grass','Grass','Grass','Mountain','Grass','BaseTaylor']
                 ]
     
+    app.characters = []
+
     app.mode = 'titleScreen'
     app.border = 30
     app.squareLength = (app.height - 2 * app.border) // 6 #len(app.board)
@@ -21,66 +21,136 @@ def appStarted(app):
     app.cols = 6 #len(app.board[0])
 
     #loading images
-    app.grassBackground = app.loadImage('grass.png')
+    app.grassBackground = app.loadImage('Images/tiles/grassland/grassland_tile.png')
     app.grassBackground = app.scaleImage(app.grassBackground, app.squareLength/100)
-    app.warriorBen = app.loadImage('warriorben.png')
-    app.warriorBen = app.scaleImage(app.warriorBen, app.squareLength/100)
+    app.waterBackground = app.loadImage('Images/tiles/ocean/water_tile.png')
+    app.waterBackground = app.scaleImage(app.waterBackground, app.squareLength/100)
+    app.mountainBackground = app.loadImage('Images/tiles/mountains/mountain_tile.png')
+    app.mountainBackground = app.scaleImage(app.mountainBackground, app.squareLength/100)
+    app.baseK = app.loadImage('Images/tiles/bases/baseK.png')
+    app.baseK = app.scaleImage(app.baseK, app.squareLength/100)
+    app.baseT = app.loadImage('Images/tiles/bases/baseT.png')
+    app.baseT = app.scaleImage(app.baseT, app.squareLength/100)
+    app.outpostK = app.loadImage('Images/tiles/outposts/kosbie_outpost.png')
+    app.outpostK = app.scaleImage(app.outpostK, app.squareLength/100)
+    app.outpostT = app.loadImage('Images/tiles/outposts/taylor_outpost.png')
+    app.outpostT = app.scaleImage(app.outpostT, app.squareLength/100)
+    app.outpost = app.loadImage('Images/tiles/outposts/outpost.png')
+    app.outpost = app.scaleImage(app.outpost, app.squareLength/100)
+    app.farmK = app.loadImage('Images/tiles/farm/kosbie_farm.png')
+    app.farmK = app.scaleImage(app.farmK, app.squareLength/100)
+    app.farmT = app.loadImage('Images/tiles/farm/taylor_farm.png')
+    app.farmT = app.scaleImage(app.farmT, app.squareLength/100)
+    app.farm = app.loadImage('Images/tiles/farm/farm.png')
+    app.farm = app.scaleImage(app.farm, app.squareLength/100)
+
+    app.archerK = app.loadImage('Images/tiles/TA sprites/archers/alex_archer.png')
+    app.archerK = app.scaleImage(app.archerK, app.squareLength/100)
+    app.archerT = app.loadImage('Images/tiles/TA sprites/archers/asad_archer.png')
+    app.archerT = app.scaleImage(app.archerT, app.squareLength/100)
+    app.knightK = app.loadImage('Images/tiles/TA sprites/horseriders/zhara_horserider.png')
+    app.knightK = app.scaleImage(app.knightK, app.squareLength/100)
+    
+    #app.warriorBen = app.loadImage('warriorben.png')
+    #app.warriorBen = app.scaleImage(app.warriorBen, app.squareLength/100)
 
 def gameMode_redrawAll(app,canvas):
     drawBoard(app,canvas)
     drawCharacters(app,canvas)
 
 def drawCharacters(app,canvas):
-    pass
+    for character in app.characters:
+        if type(character) == Archer:
+            if character.owner == 'Kosbie':
+                canvas.create_image(character.x * app.squareLength + app.border + app.squareLength // 2,
+                                    character.y * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.grassBackground))
+            else:
+                canvas.create_image(character.x * app.squareLength + app.border + app.squareLength // 2,
+                                    character.y * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.grassBackground))
+        
+        if type(character) == Soldier:
+            if character.owner == 'Kosbie':
+                canvas.create_image(character.x * app.squareLength + app.border + app.squareLength // 2,
+                                    character.y * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.grassBackground))
+            else:
+                canvas.create_image(character.x * app.squareLength + app.border + app.squareLength // 2,
+                                    character.y * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.grassBackground))
+
+        if type(character) == Knight:
+            if character.owner == 'Kosbie':
+                canvas.create_image(character.x * app.squareLength + app.border + app.squareLength // 2,
+                                    character.y * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.grassBackground))
+            else:
+                canvas.create_image(character.x * app.squareLength + app.border + app.squareLength // 2,
+                                    character.y * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.grassBackground))
+
 
 #draws images for each terrain
 def drawBoard(app,canvas):
     for row in range(6):
         for col in range(6):
-            if app.board[row][col] == 'Grass':
+            if app.board[col][row] == 'Grass':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
                                     image=ImageTk.PhotoImage(app.grassBackground))
             
-            elif app.board[row][col] == 'Ocean':
+            elif app.board[col][row] == 'Ocean':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.waterBackground))
 
-            elif app.board[row][col] == 'Mountain':
+            elif app.board[col][row] == 'Mountain':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.mountainBackground))
 
-            elif app.board[row][col] == 'Outpost':
+            elif app.board[col][row] == 'Outpost':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.outpost))
             
-            elif app.board[row][col] == 'OutpostTaylor':
+            elif app.board[col][row] == 'OutpostTaylor':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.outpostT))
 
-            elif app.board[row][col] == 'OutPostKosbie':
+            elif app.board[col][row] == 'OutpostKosbie':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.outpostK))
             
-            elif app.board[row][col] == 'Farm':
+            elif app.board[col][row] == 'Farm':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.farm))
 
-            elif app.board[row][col] == 'FarmTaylor':
+            elif app.board[col][row] == 'FarmTaylor':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.farmT))
             
-            elif app.board[row][col] == 'FarmKosbie':
+            elif app.board[col][row] == 'FarmKosbie':
                 canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
                                     col * app.squareLength + app.border + app.squareLength //2,
-                                    image=ImageTk.PhotoImage(app.grassBackground))
+                                    image=ImageTk.PhotoImage(app.farmK))
+            
+            elif app.board[col][row] == 'BaseTaylor':
+                canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
+                                    col * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.baseT))
+            
+            elif app.board[col][row] == 'BaseKosbie':
+                canvas.create_image(row * app.squareLength + app.border + app.squareLength // 2,
+                                    col * app.squareLength + app.border + app.squareLength //2,
+                                    image=ImageTk.PhotoImage(app.baseK))
+            
+           
 
 
 
