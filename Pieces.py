64bf3b98acfc2c3cee96a328025f4dd4eeab87da
anirudhprakash <anirudh.prakash1@gmail.com>
2021-11-07 13:39:49 -0500

    Update Pieces.py

--- Pieces.py
+++ Pieces.py
@@ -8,7 +8,7 @@ class Piece(object):
         self.owner = owner
     
     def attack (self, other):
-        other.health -= self.attack
+        other.health -= self.attackDamage
 
     def getX (self):
         return self.x
@@ -21,7 +21,7 @@ class Piece(object):
 
     def getPieceInCell(self,row,col,app):
         for otherCharacter in app.characters:
-                if otherCharacter.getCords == (row,col):
+                if otherCharacter.getCords() == (col,row):
                     return otherCharacter
         return None
 
@@ -45,24 +45,48 @@ class Piece(object):
     def move(self,app,targetRow,targetCol):
         
         enemyPiece = self.getPieceInCell(targetRow,targetCol,app)          #if enemy in targetRow, targetCol -> attack:
+        print(enemyPiece)
         if enemyPiece != None:
-            self.attack(self,enemyPiece)
+            self.attack(enemyPiece)
             if enemyPiece.health <= 0:
                 app.characters.remove(enemyPiece)
 
-        enemyPiece = self.getPieceInCell(targetRow,targetCol,app)
+            
+        if self.getPieceInCell(targetRow,targetCol,app) == None: 
+            self.x = targetCol
+            self.y = targetRow
+        
         if enemyPiece == None: 
+            if 'outpost' in app.board[targetRow][targetCol].lower():
+                app.board[targetRow][targetCol] = "Outpost" + self.owner
+
+            if 'farm' in app.board[targetRow][targetCol].lower():
+                app.board[targetRow][targetCol] = "Farm" + self.owner
+
             self.x = targetCol
             self.y = targetRow
 
+        
+        for dx in range(-1,2):                                                  #UPDATE THIS TO CODE
+            for dy in range(-1,2):
+                newX = self.x + dx
+                newY = self.y + dy
+
+                if newX < 0 or newX >= len(app.board) or newY < 0 or newY >= len(app.board):
+                    continue
+                if app.visibility[self.y+dy][self.x+dx] == 0:
+                    app.visibility[self.y+dy][self.x+dx] = 1
+                if app.visibility[self.y+dy][self.x+dx] == 2:
+                    app.visibility[self.y+dy][self.x+dx] = 3
+
 class Soldier(Piece):
     def __init__(self,x,y,owner):
-        super().__init__(x,y,25,1,owner,50)
+        super().__init__(x,y,250,1,owner,50)
 
 class Knight(Piece):
     def __init__(self,x,y,owner):
-        super().__init__(x,y,40,2,owner,75)
+        super().__init__(x,y,400,2,owner,1)
 
 class Archer(Piece):
     def __init__(self,x,y,owner):
-        super().__init__(x,y,30,1,owner,40)
+        super().__init__(x,y,300,1,owner,40)
