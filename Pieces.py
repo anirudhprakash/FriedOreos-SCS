9d6b8ca1adbe4c828517f278dadb64d54185cffa
Bhavik Thati <56490804+Bhavik-T@users.noreply.github.com>
2021-11-07 14:27:17 -0500

    Update Pieces.py

--- Pieces.py
+++ Pieces.py
@@ -27,18 +27,42 @@ class Piece(object):
 
     def generateMovesAllowed(self,app):
         generatedMovesAllowed = []
-        for dx in range((-1*self.movesAllowed), (self.movesAllowed)+1):
-            for dy in range((-1*self.movesAllowed), (self.movesAllowed)+1):
-                if dx == 0 and dy == 0:
-                    continue
-                newX = self.x + dx
-                newY = self.y + dy
+        if(isinstance(self,Archer)):
+            for dx in range((-1*self.movesAllowed)-1, (self.movesAllowed)+2):
+                for dy in range((-1*self.movesAllowed)-1, (self.movesAllowed)+2):
+                    if dx == 0 and dy == 0:
+                        continue
+                    newX = self.x + dx
+                    newY = self.y + dy
+                    if newX < 0 or newX >= len(app.board) or newY < 0 or newY >= len(app.board):
+                        continue
+                    if (dx < (-1*self.movesAllowed) or dx > (self.movesAllowed)) or (dy < (-1*self.movesAllowed) or dy > (self.movesAllowed)):
+                            pieceInCell = self.getPieceInCell(newY, newX, app)
+                            if pieceInCell != None and pieceInCell.owner != self.owner:
+                                generatedMovesAllowed.append((newY,newX))
+                    else:
+                        pieceInCell = self.getPieceInCell(newY,newX,app)
+                        if pieceInCell != None and pieceInCell.owner == self.owner:
+                            continue
+                        if app.board[self.y+dy][self.x + dx] not in ["Ocean", "Mountain"]:
+                            generatedMovesAllowed.append((self.y+dy,self.x+dx))
+        else:
+            for dx in range((-1*self.movesAllowed), (self.movesAllowed)+1):
+                for dy in range((-1*self.movesAllowed), (self.movesAllowed)+1):
+                    if dx == 0 and dy == 0:
+                        continue
+                    newX = self.x + dx
+                    newY = self.y + dy
 
-                if newX < 0 or newX >= len(app.board) or newY < 0 or newY >= len(app.board):
-                    continue
+                    if newX < 0 or newX >= len(app.board) or newY < 0 or newY >= len(app.board):
+                        continue
 
-                if app.board[self.y+dy][self.x + dx] not in ["Ocean", "Mountain"]:
-                    generatedMovesAllowed.append((self.y+dy,self.x+dx))
+                    pieceInCell = self.getPieceInCell(newY,newX,app)
+                    if pieceInCell != None and pieceInCell.owner == self.owner:
+                        continue
+
+                    if app.board[self.y+dy][self.x + dx] not in ["Ocean", "Mountain"]:
+                        generatedMovesAllowed.append((self.y+dy,self.x+dx))
     
         return generatedMovesAllowed
     
@@ -46,7 +70,7 @@ class Piece(object):
         
         enemyPiece = self.getPieceInCell(targetRow,targetCol,app)          #if enemy in targetRow, targetCol -> attack:
         print(enemyPiece)
-        if enemyPiece != None:
+        if enemyPiece != None and enemyPiece.owner != self.owner:
             self.attack(enemyPiece)
             if enemyPiece.health <= 0:
                 app.characters.remove(enemyPiece)
@@ -67,17 +91,16 @@ class Piece(object):
             self.y = targetRow
 
         
-        for dx in range(-1,2):                                                  #UPDATE THIS TO CODE
+        for dx in range(-1,2):                                                  
             for dy in range(-1,2):
                 newX = self.x + dx
                 newY = self.y + dy
 
                 if newX < 0 or newX >= len(app.board) or newY < 0 or newY >= len(app.board):
                     continue
-                if app.visibility[self.y+dy][self.x+dx] == 0:
-                    app.visibility[self.y+dy][self.x+dx] = 1
-                if app.visibility[self.y+dy][self.x+dx] == 2:
-                    app.visibility[self.y+dy][self.x+dx] = 3
+                if app.visibility[self.y+dy][self.x+dx] < 3 and app.visibility[self.y+dy][self.x+dx] != app.turn+1  :
+                    app.visibility[self.y+dy][self.x+dx] += app.turn + 1
+
 
 class Soldier(Piece):
     def __init__(self,x,y,owner):
