d62c6d7b8d4fab6c1ca013c01959777883da7470
anirudhprakash <anirudh.prakash1@gmail.com>
2021-11-06 20:12:52 -0400

    Update gameRules.py

--- gameRules.py
+++ gameRules.py
@@ -59,9 +59,6 @@ def generateMap(dimensions):
 
             if isBorder == True:
                 probOutpost = probOutpostInterior
-
-
-
 from collections import deque
 
 
@@ -86,28 +83,23 @@ def checkMap(board):
                 return False
     return True
 
-#TODO Modify this BFS to my BFS
-def bfs(grid, start):
-    queue = collections.deque([[start]])
-    seen = set([start])
-    while queue:
-        path = queue.popleft()
-        x, y = path[-1]
-        if grid[y][x] == goal:
-            return path
-        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-            if 0 <= x2 < width and 0 <= y2 < height and grid[y2][x2] != wall and (x2, y2) not in seen:
-                queue.append(path + [(x2, y2)])
-                seen.add((x2, y2))
-
 
 def BFS(start, end, graph):
     queue = dequeu(start)
     seen = set([start])
+    while queue:
+        path = queue.popleft()
+        if len(path) > 20:
+            return False
+        x, y = path[-1]
+        if x == end[0] and y == end[1]:
+            return True
+        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+            if 0 <= x2 < len(graph) and 0 <= y2 < len(graph) and graph[x2][y2] != 120 and (x2, y2) not in seen:
+                queue.append(path + [(x2, y2)])
+                seen.add((x2, y2))
 
 
-
-    
 
 
 
